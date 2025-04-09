fb76b7a60afdf4b420136b1c560d8d3f1b524812
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-04-10 01:38:39 +0530

    minor change

--- main/components/Footer.tsx
+++ main/components/Footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
 
             <div className="mt-8 items-center justify-between sm:flex">
                 <div className="mt-4 sm:mt-0">
-                    &copy; 2024 Dinesh Sutihar All rights reserved.
+                    &copy; 2025 Dinesh Sutihar All rights reserved.
                 </div>
                 <div className="mt-6 sm:mt-0">
                     <ul className="flex items-center space-x-4">
