481089c342e32c802095977158f7b60b10eb0575
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-05-22 04:03:52 +0530

    style: main new grid style modify

--- main/components/Grid2.tsx
+++ main/components/Grid2.tsx
@@ -4,13 +4,15 @@ import CodingAchievements from "./CodingAchievements";
 import Timeline from "./Timeline";
 import GitHubContributions from "./GitHubContributions";
 import RecentCommits from "./RecentCommits";
+import LeetCodeStats from "./LeetCodeStats";
 
 export const Grid2 = () => {
     return (
-        <section id="about" className="grid grid-cols-1 md:grid-cols-5 gap-6">
+        <section id="about" className="grid grid-cols-1 md:grid-cols-5 md:grid-rows-3 gap-6">
             <Timeline details={education} />
             <RecentCommits />
-            {/* <GitHubContributions /> */}
+            <LeetCodeStats />
+            <GitHubContributions />
             {/* <CodingAchievements /> */}
         </section>
     )
