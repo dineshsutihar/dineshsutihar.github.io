1d652be016db0a4f75edce9b786c2267cd2b2151
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-05-23 04:35:43 +0530

    backend: add leetcode route to fetch data using graphql

---
+++ main/app/api/leetcode/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const { query, variables } = body;
+
+    const response = await fetch("https://leetcode.com/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Referer: `https://leetcode.com/${variables?.username || ""}/`,
+      },
+      body: JSON.stringify({ query, variables }),
+    });
+
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (err) {
+    console.error("Error fetching from LeetCode:", err);
+    return new Response(
+      JSON.stringify({ message: "Internal Server Error" }),
+      { status: 500 }
+    );
+  }
+}
