eceeb93d9b82b3f8d2226b9bf2c2698266c964b2
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-05-24 04:42:57 +0530

    build: remove unnecessary packages

--- main/components/Grid2.tsx
+++ main/components/Grid2.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { education } from "@/data";
-import CodingAchievements from "./CodingAchievements";
+// import CodingAchievements from "./CodingAchievements";
 import Timeline from "./Timeline";
 import GitHubContributions from "./GitHubContributions";
 import RecentCommits from "./RecentCommits";
@@ -8,7 +8,7 @@ import LeetCodeStats from "./LeetCodeStats";
 
 export const Grid2 = () => {
     return (
-        <section id="about" className="grid grid-cols-1 md:grid-cols-5 md:grid-rows-3 gap-6">
+        <section id="about" className="grid grid-cols-1 md:grid-cols-5 gap-6"> {/* Later grid row should be added and grid2 name should be changed*/}
             <Timeline details={education} />
             <RecentCommits />
             <LeetCodeStats />
