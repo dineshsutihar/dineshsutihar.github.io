7310952ed56f18c185026d74a27652622aa275fc
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2025-05-26 13:28:21 +0530

    style: all the grid style are on grid2

--- main/components/Grid2.tsx
+++ main/components/Grid2.tsx
@@ -1,19 +1,40 @@
-import React from "react"
+"use client";
 import { education } from "@/data";
-// import CodingAchievements from "./CodingAchievements";
 import Timeline from "./Timeline";
 import GitHubContributions from "./GitHubContributions";
 import RecentCommits from "./RecentCommits";
+
+import { LeetCodeContest } from "./LeetCodeContest";
+import { LeetCodeHeatmap } from "./LeetCodeHeatmap";
 import LeetCodeStats from "./LeetCodeStats";
+import { CTAButton } from "./CTAButton";
 
 export const Grid2 = () => {
     return (
-        <section id="about" className="grid grid-cols-1 md:grid-cols-5 gap-6"> {/* Later grid row should be added and grid2 name should be changed*/}
-            <Timeline details={education} />
-            <RecentCommits />
-            <LeetCodeStats />
-            <GitHubContributions />
-            {/* <CodingAchievements /> */}
+        <section id="about" className="grid grid-cols-1 md:grid-cols-5 gap-6">
+
+            <div className="col-span-1 md:col-span-3">
+                <Timeline details={education} />
+            </div>
+            <div className="col-span-1 md:col-span-2">
+                <RecentCommits />
+            </div>
+            <div className="col-span-1 md:col-span-2 flex items-center justify-center">
+                <CTAButton />
+            </div>
+
+            <div className="col-span-1 md:col-span-3">
+                <GitHubContributions />
+            </div>
+            <div className="col-span-1 md:col-span-3">
+                <LeetCodeContest />
+            </div>
+            <div className="col-span-1 md:col-span-2">
+                <LeetCodeStats />
+            </div>
+            <div className="col-span-1 md:col-span-4">
+                <LeetCodeHeatmap />
+            </div>
         </section>
-    )
-}
+    );
+};
